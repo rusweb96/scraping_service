8223b735b423a6ce352cf2bad7bcf24b15c49eab
rusweb96 <rusweb96@gmail.com>
2022-10-13 23:40:55 +0600

    Первый Комид

---
+++ scraping/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+from .utils import from_cyrillic_to_eng
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Названия города',
+                            unique=True)
+
+    slug = models.CharField(max_length=50, blank=True,
+                            unique=True)
+
+    class Meta:
+        verbose_name = 'Названия города'
+        verbose_name_plural = 'Название городов'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+
+        super().save(*args, **kwargs)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Язык програмирование',
+                            unique=True)
+
+    slug = models.CharField(max_length=50, blank=True,
+                            unique=True)
+
+    class Meta:
+        verbose_name = 'Язык програмирование'
+        verbose_name_plural = 'Языки програмирования'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+
+        super().save(*args, **kwargs)
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True, )
+    title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    language = models.ForeignKey('language', on_delete=models.CASCADE, verbose_name='Язык програмирование')
+    timestamp = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+
+    def __str__(self):
+        return self.title
---
+++ scraping_service/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+import datetime
+
+def home(request):
+    name = 'Руся'
+    date = datetime.datetime.now().date()
+    context = {'name': name, 'data': date}
+    return render(request, 'home.html', context)
