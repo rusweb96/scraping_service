5b60f451936201fafe08e1f1cba225363764f7fc
rusweb96 <rusweb96@gmail.com>
2022-10-13 23:55:26 +0600

    Новый директория внутри папки Templates

--- scraping/views.py
+++ scraping/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from  .models import Vacancy
+def home_view(request):
+    qs = Vacancy.objects.all()
+    return render(request, 'home,html', {'object_list': qs})
